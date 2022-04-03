2e215b79fa2102e70af30d3630a4b12acbd0c252
vejron <bjorn.yttergren@gmail.com>
2022-04-03 16:19:18 +0200

    fix: news listing

--- src/components/ArticleListing.vue
+++ src/components/ArticleListing.vue
@@ -3,10 +3,10 @@
     class="flex flex-col gap-8"
   >
     <ul v-for="post in sorted()" :key="post.url">
-      <li class="group relative overflow-hidden flex gap-8">
+      <li class="group relative overflow-hidden flex flex-col sm:flex-row gap-8">
         <a :href="post.url">
           <img
-            class="transition-transform min-h-full duration-700 max-w-xs group-hover:scale-105 object-cover"
+            class="transition-transform min-h-full duration-700 w-full sm:max-w-xs sm:w-auto group-hover:scale-105 object-cover"
             style="aspect-ratio: 1.618 / 1"
             :src="post.thumbnail"
             alt=""

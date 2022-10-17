0180687120de76e2df8976f1de4ea1268c6a154e
vejron <bjorn.yttergren@gmail.com>
2022-10-17 20:42:10 +0200

    fix: update all

--- src/components/Anmalan.vue
+++ src/components/Anmalan.vue
@@ -4,26 +4,35 @@
     <form @submit.prevent="submitForm" class="grid grid-cols-1 sm:grid-cols-2 gap-y-2 gap-x-6">
       <div class="flex flex-col">
         <label for="id-name">Namn</label>
-        <input v-model="formData.name" id="id-name" type="text" />
-      </div>
-      <div class="flex flex-col">
-        <label for="id-mail">E-post</label>
-        <input v-model="formData.email" id="id-mail" type="email" />
+        <input required v-model="formData.name" id="id-name" type="text" />
       </div>
       <div class="flex flex-col">
         <label for="id-klubb">Klubb</label>
         <input v-model="formData.club" id="id-klubb" type="text" />
       </div>
       <div class="flex flex-col">
+        <label for="id-klubb">Vasa-ID</label>
+        <input v-model="formData.vasa" id="id-klubb" type="text" />
+      </div>
+      <div class="flex flex-col">
+        <label for="id-mail">E-post</label>
+        <input required v-model="formData.email" id="id-mail" type="email" />
+      </div>
+
+      <div class="flex flex-col">
+        <label for="id-klass">Sträcka</label>
+        <select required v-model="formData.distance" id="id-klass">
+          <option v-for="distance in distances" :value="distance.value">{{ distance.key }}</option>
+        </select>
+      </div>
+      <div v-if="formData.distance" class="flex flex-col">
         <label for="id-klass">Klass</label>
-        <select v-model="formData.class" id="id-klass">
+        <select required v-model="formData.class" id="id-klass">
           <option v-for="klass in klasser" :value="klass">{{ klass }}</option>
         </select>
       </div>
       <div class="flex sm:col-span-2 justify-end mt-4">
-        <button
-          class="px-4 py-2 bg-green-500 rounded-md text-white text-2xl font-semibold"
-        >
+        <button type="submit" class="px-4 py-2 bg-green-500 rounded-md text-white text-2xl font-semibold">
           Anmälan
         </button>
       </div>
@@ -32,59 +41,73 @@
 </template>
 
 <script setup lang="ts">
-import { reactive } from "vue";
-import { loadStripe } from "@stripe/stripe-js";
+import { reactive, computed } from "vue";
 
 const submitForm = async () => {
   return;
-  const data = {
-    sku: "DEMO001",
-    quantity: 1,
-  };
 
-  const response = await fetch("/.netlify/functions/create-checkout", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data),
-  }).then((res) => res.json());
-
-  const stripe = await loadStripe(response.publishableKey);
-  const { error } = await stripe.redirectToCheckout({
-    sessionId: response.sessionId,
-  });
-
-  if (error) {
-    console.error(error);
-  }
 };
 
 const formData = reactive({
-  name: "John doe",
+  name: "",
   email: "",
-  club: "TTGU",
-  class: "Motion 40Km",
+  club: "",
+  class: "",
+  distance: "",
+  vasa: ""
 });
 
-const klasser = [
-  "Motion 20Km",
-  "Motion 40Km",
-  "D21",
-  "D30",
-  "D35",
-  "D40",
-  "D45",
-  "D50",
-  "D60",
-  "D70",
-  "H21",
-  "H30",
-  "H35",
-  "H40",
-  "H45",
-  "H50",
-  "H60",
-  "H70",
-];
+const klasser = computed(() => {
+  return distances.find(d => d.value === formData.distance)?.classes
+})
+
+const distances = [
+  {
+    value: 'long',
+    key: '40 km',
+    classes: [
+      'Herrar Motion',
+      'Damer Motion',
+      'D21',
+      'D30',
+      'D35',
+      'D40',
+      'D45',
+      'D50',
+      'D55',
+      'D60',
+      'D70',
+      'D80',
+      'D90',
+      'H21',
+      'H30',
+      'H35',
+      'H40',
+      'H45',
+      'H50',
+      'H55',
+      'H60',
+      'H70',
+      'H80',
+      'H90',
+    ]
+  },
+  {
+    value: 'short',
+    key: '20 km',
+    classes: [
+      'Herrar Motion',
+      'Damer Motion',
+    ]
+  },
+  {
+    value: 'para',
+    key: '10 km Paraklass',
+    classes: [
+      'Herrar Motion',
+      'Damer Motion',
+    ]
+  },
+]
+
 </script>
--- src/components/ArticleListing.vue
+++ src/components/ArticleListing.vue
@@ -1,21 +1,16 @@
 <template>
-  <section
-    class="flex flex-col gap-8"
-  >
-    <ul v-for="post in sorted()" :key="post.url">
+  <section class="flex flex-col gap-8">
+    <ul v-for="post in posts" :key="post.frontmatter.url">
       <li class="group relative overflow-hidden flex flex-col sm:flex-row gap-8">
-        <a :href="post.url">
+        <a :href="post.frontmatter.url">
           <img
             class="transition-transform min-h-full duration-700 w-full sm:max-w-xs sm:w-auto group-hover:scale-105 object-cover"
-            style="aspect-ratio: 1.618 / 1"
-            :src="post.thumbnail"
-            alt=""
-          />
-          
+            style="aspect-ratio: 1.618 / 1" :src="post.frontmatter.thumbnail" alt="" />
+
         </a>
         <div>
-            <h3 class="font-semibold text-xl">{{ post.displaytitle }}</h3>
-            <p>{{post.description}}</p>
+          <h3 class="font-semibold text-xl">{{ post.frontmatter.displaytitle }}</h3>
+          <p>{{post.frontmatter.description}}</p>
         </div>
       </li>
     </ul>
@@ -28,6 +23,6 @@ const props = defineProps({
 });
 
 const sorted = () => {
-  return props.posts.sort((a, b) => new Date(b.date) - new Date(a.date) );
+  return props.posts.sort((a, b) => new Date(b.frontmatter.date) - new Date(a.frontmatter.date));
 }
 </script>
